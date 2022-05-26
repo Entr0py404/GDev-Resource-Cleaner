361c865d0673714e95453e89142e3911a6d9325d
Entropy <devfire404@gmail.com>
2022-05-26 08:25:38 -0700

    update

--- GDev Resource Cleaner/Form1.Designer.vb
+++ GDev Resource Cleaner/Form1.Designer.vb
@@ -28,19 +28,26 @@ Partial Class Form1
         Me.Button_MoveToRemove = New System.Windows.Forms.Button()
         Me.Button_SelectProjectJSON = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.ListBox_UnesedResources = New System.Windows.Forms.ListBox()
+        Me.ListBox_UnusedResources = New System.Windows.Forms.ListBox()
         Me.Label_Resources = New System.Windows.Forms.Label()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.Button_Reload = New System.Windows.Forms.Button()
+        Me.Button_Close = New System.Windows.Forms.Button()
         Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.ShapeContainer3 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label_UnesedResources = New System.Windows.Forms.Label()
+        Me.Label_UnusedResources = New System.Windows.Forms.Label()
         Me.ShapeContainer2 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
         Me.RectangleShape2 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox_ProjectFilePath = New System.Windows.Forms.TextBox()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.RectangleShape3 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'ListBox_Resources
@@ -63,9 +70,9 @@ Partial Class Form1
         Me.Button_MoveToRemove.FlatAppearance.BorderSize = 0
         Me.Button_MoveToRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button_MoveToRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_MoveToRemove.Location = New System.Drawing.Point(408, 472)
+        Me.Button_MoveToRemove.Location = New System.Drawing.Point(400, 536)
         Me.Button_MoveToRemove.Name = "Button_MoveToRemove"
-        Me.Button_MoveToRemove.Size = New System.Drawing.Size(192, 31)
+        Me.Button_MoveToRemove.Size = New System.Drawing.Size(200, 32)
         Me.Button_MoveToRemove.TabIndex = 1
         Me.Button_MoveToRemove.Text = "Move To Remove Folder"
         Me.Button_MoveToRemove.UseVisualStyleBackColor = False
@@ -78,11 +85,11 @@ Partial Class Form1
         Me.Button_SelectProjectJSON.FlatAppearance.BorderSize = 0
         Me.Button_SelectProjectJSON.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button_SelectProjectJSON.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_SelectProjectJSON.Location = New System.Drawing.Point(8, 472)
+        Me.Button_SelectProjectJSON.Location = New System.Drawing.Point(432, 24)
         Me.Button_SelectProjectJSON.Name = "Button_SelectProjectJSON"
-        Me.Button_SelectProjectJSON.Size = New System.Drawing.Size(168, 32)
+        Me.Button_SelectProjectJSON.Size = New System.Drawing.Size(64, 24)
         Me.Button_SelectProjectJSON.TabIndex = 2
-        Me.Button_SelectProjectJSON.Text = "Select Project JSON"
+        Me.Button_SelectProjectJSON.Text = "..."
         Me.ToolTip1.SetToolTip(Me.Button_SelectProjectJSON, "You can drag & drop a project json file here.")
         Me.Button_SelectProjectJSON.UseVisualStyleBackColor = False
         '
@@ -90,18 +97,18 @@ Partial Class Form1
         '
         Me.OpenFileDialog1.Filter = "Json files (*.json)|*.json"
         '
-        'ListBox_UnesedResources
+        'ListBox_UnusedResources
         '
-        Me.ListBox_UnesedResources.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(54, Byte), Integer))
-        Me.ListBox_UnesedResources.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.ListBox_UnesedResources.ForeColor = System.Drawing.Color.WhiteSmoke
-        Me.ListBox_UnesedResources.FormattingEnabled = True
-        Me.ListBox_UnesedResources.HorizontalScrollbar = True
-        Me.ListBox_UnesedResources.Location = New System.Drawing.Point(16, 32)
-        Me.ListBox_UnesedResources.Name = "ListBox_UnesedResources"
-        Me.ListBox_UnesedResources.Size = New System.Drawing.Size(576, 182)
-        Me.ListBox_UnesedResources.TabIndex = 4
-        Me.ToolTip1.SetToolTip(Me.ListBox_UnesedResources, "Double click item to remove it from list.")
+        Me.ListBox_UnusedResources.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.ListBox_UnusedResources.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListBox_UnusedResources.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.ListBox_UnusedResources.FormattingEnabled = True
+        Me.ListBox_UnusedResources.HorizontalScrollbar = True
+        Me.ListBox_UnusedResources.Location = New System.Drawing.Point(16, 32)
+        Me.ListBox_UnusedResources.Name = "ListBox_UnusedResources"
+        Me.ListBox_UnusedResources.Size = New System.Drawing.Size(576, 182)
+        Me.ListBox_UnusedResources.TabIndex = 4
+        Me.ToolTip1.SetToolTip(Me.ListBox_UnusedResources, "Double click item to remove it from list.")
         '
         'Label_Resources
         '
@@ -125,13 +132,28 @@ Partial Class Form1
         Me.Button_Reload.FlatAppearance.BorderSize = 0
         Me.Button_Reload.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button_Reload.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_Reload.Location = New System.Drawing.Point(184, 472)
+        Me.Button_Reload.Location = New System.Drawing.Point(8, 536)
         Me.Button_Reload.Name = "Button_Reload"
-        Me.Button_Reload.Size = New System.Drawing.Size(96, 32)
+        Me.Button_Reload.Size = New System.Drawing.Size(112, 32)
         Me.Button_Reload.TabIndex = 9
-        Me.Button_Reload.Text = "Reload"
+        Me.Button_Reload.Text = "Scan Files"
         Me.ToolTip1.SetToolTip(Me.Button_Reload, "You can drag drop a project json file here.")
         Me.Button_Reload.UseVisualStyleBackColor = False
+        '
+        'Button_Close
+        '
+        Me.Button_Close.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button_Close.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.Button_Close.FlatAppearance.BorderSize = 0
+        Me.Button_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_Close.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_Close.Location = New System.Drawing.Point(520, 24)
+        Me.Button_Close.Name = "Button_Close"
+        Me.Button_Close.Size = New System.Drawing.Size(80, 24)
+        Me.Button_Close.TabIndex = 11
+        Me.Button_Close.Text = "Close"
+        Me.ToolTip1.SetToolTip(Me.Button_Close, "You can drag drop a project json file here.")
+        Me.Button_Close.UseVisualStyleBackColor = False
         '
         'RectangleShape1
         '
@@ -151,7 +173,7 @@ Partial Class Form1
         Me.Panel1.Controls.Add(Me.Label_Resources)
         Me.Panel1.Controls.Add(Me.ListBox_Resources)
         Me.Panel1.Controls.Add(Me.ShapeContainer3)
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Location = New System.Drawing.Point(0, 64)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(608, 232)
         Me.Panel1.TabIndex = 7
@@ -168,23 +190,23 @@ Partial Class Form1
         '
         'Panel2
         '
-        Me.Panel2.Controls.Add(Me.Label_UnesedResources)
-        Me.Panel2.Controls.Add(Me.ListBox_UnesedResources)
+        Me.Panel2.Controls.Add(Me.Label_UnusedResources)
+        Me.Panel2.Controls.Add(Me.ListBox_UnusedResources)
         Me.Panel2.Controls.Add(Me.ShapeContainer2)
-        Me.Panel2.Location = New System.Drawing.Point(0, 232)
+        Me.Panel2.Location = New System.Drawing.Point(0, 296)
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(608, 232)
         Me.Panel2.TabIndex = 8
         '
-        'Label_UnesedResources
+        'Label_UnusedResources
         '
-        Me.Label_UnesedResources.AutoSize = True
-        Me.Label_UnesedResources.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label_UnesedResources.Location = New System.Drawing.Point(8, 8)
-        Me.Label_UnesedResources.Name = "Label_UnesedResources"
-        Me.Label_UnesedResources.Size = New System.Drawing.Size(114, 13)
-        Me.Label_UnesedResources.TabIndex = 9
-        Me.Label_UnesedResources.Text = "Unesed Resources"
+        Me.Label_UnusedResources.AutoSize = True
+        Me.Label_UnusedResources.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_UnusedResources.Location = New System.Drawing.Point(8, 8)
+        Me.Label_UnusedResources.Name = "Label_UnusedResources"
+        Me.Label_UnusedResources.Size = New System.Drawing.Size(114, 13)
+        Me.Label_UnusedResources.TabIndex = 9
+        Me.Label_UnusedResources.Text = "Unused Resources"
         '
         'ShapeContainer2
         '
@@ -209,17 +231,73 @@ Partial Class Form1
         Me.RectangleShape2.SelectionColor = System.Drawing.Color.Transparent
         Me.RectangleShape2.Size = New System.Drawing.Size(592, 200)
         '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Button_Close)
+        Me.Panel3.Controls.Add(Me.Button_SelectProjectJSON)
+        Me.Panel3.Controls.Add(Me.Label1)
+        Me.Panel3.Controls.Add(Me.TextBox_ProjectFilePath)
+        Me.Panel3.Controls.Add(Me.ShapeContainer1)
+        Me.Panel3.Location = New System.Drawing.Point(0, 8)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(608, 56)
+        Me.Panel3.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Project File Path"
+        '
+        'TextBox_ProjectFilePath
+        '
+        Me.TextBox_ProjectFilePath.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.TextBox_ProjectFilePath.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox_ProjectFilePath.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.TextBox_ProjectFilePath.Location = New System.Drawing.Point(16, 32)
+        Me.TextBox_ProjectFilePath.Name = "TextBox_ProjectFilePath"
+        Me.TextBox_ProjectFilePath.ReadOnly = True
+        Me.TextBox_ProjectFilePath.Size = New System.Drawing.Size(392, 13)
+        Me.TextBox_ProjectFilePath.TabIndex = 0
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape3})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(608, 56)
+        Me.ShapeContainer1.TabIndex = 1
+        Me.ShapeContainer1.TabStop = False
+        '
+        'RectangleShape3
+        '
+        Me.RectangleShape3.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.RectangleShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.RectangleShape3.BorderColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.RectangleShape3.CornerRadius = 3
+        Me.RectangleShape3.FillColor = System.Drawing.Color.WhiteSmoke
+        Me.RectangleShape3.FillGradientColor = System.Drawing.Color.WhiteSmoke
+        Me.RectangleShape3.Location = New System.Drawing.Point(8, 24)
+        Me.RectangleShape3.Name = "RectangleShape3"
+        Me.RectangleShape3.SelectionColor = System.Drawing.Color.Transparent
+        Me.RectangleShape3.Size = New System.Drawing.Size(408, 24)
+        '
         'Form1
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(62, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(608, 512)
+        Me.ClientSize = New System.Drawing.Size(608, 577)
+        Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Button_Reload)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.Button_SelectProjectJSON)
         Me.Controls.Add(Me.Button_MoveToRemove)
         Me.ForeColor = System.Drawing.Color.WhiteSmoke
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -233,8 +311,9 @@ Partial Class Form1
         Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
         Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -242,15 +321,21 @@ Partial Class Form1
     Friend WithEvents Button_MoveToRemove As Button
     Friend WithEvents Button_SelectProjectJSON As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
-    Friend WithEvents ListBox_UnesedResources As ListBox
+    Friend WithEvents ListBox_UnusedResources As ListBox
     Friend WithEvents Label_Resources As Label
     Friend WithEvents ToolTip1 As ToolTip
     Friend WithEvents RectangleShape1 As RectangleShape
     Friend WithEvents Panel1 As Panel
     Friend WithEvents ShapeContainer3 As ShapeContainer
     Friend WithEvents Panel2 As Panel
-    Friend WithEvents Label_UnesedResources As Label
+    Friend WithEvents Label_UnusedResources As Label
     Friend WithEvents ShapeContainer2 As ShapeContainer
     Friend WithEvents RectangleShape2 As RectangleShape
     Friend WithEvents Button_Reload As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox_ProjectFilePath As TextBox
+    Friend WithEvents ShapeContainer1 As ShapeContainer
+    Friend WithEvents RectangleShape3 As RectangleShape
+    Friend WithEvents Button_Close As Button
 End Class
--- GDev Resource Cleaner/Form1.vb
+++ GDev Resource Cleaner/Form1.vb
@@ -5,6 +5,7 @@ Public Class Form1
     Private Sub Button_SelectProjectJSON_Click(sender As Object, e As EventArgs) Handles Button_SelectProjectJSON.Click
         OpenFileDialog1.FileName = ""
         If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            TextBox_ProjectFilePath.Text = OpenFileDialog1.FileName
             ProjectJSON()
         End If
     End Sub
@@ -14,26 +15,36 @@ Public Class Form1
             ProjectJSON()
         End If
     End Sub
-    'ListBox_UnesedResources - DoubleClick
-    Private Sub ListBox_UnesedResources_DoubleClick(sender As Object, e As EventArgs) Handles ListBox_UnesedResources.DoubleClick
-        If Not ListBox_UnesedResources.SelectedIndex = -1 Then
-            ListBox_UnesedResources.Items.RemoveAt(ListBox_UnesedResources.SelectedIndex)
+    'ListBox_UnusedResources - DoubleClick
+    Private Sub ListBox_UnusedResources_DoubleClick(sender As Object, e As EventArgs) Handles ListBox_UnusedResources.DoubleClick
+        If Not ListBox_UnusedResources.SelectedIndex = -1 Then
+            ListBox_UnusedResources.Items.RemoveAt(ListBox_UnusedResources.SelectedIndex)
         End If
+    End Sub
+    'Button_Close - Click
+    Private Sub Button_Close_Click(sender As Object, e As EventArgs) Handles Button_Close.Click
+        TextBox_ProjectFilePath.Clear()
+        ListBox_Resources.Items.Clear()
+        ListBox_UnusedResources.Items.Clear()
+        Label_Resources.Text = "Resources"
+        Label_UnusedResources.Text = "Unused Resources"
     End Sub
     'Button_MoveToRemove - Click
     Private Sub Button_MoveToRemove_Click(sender As Object, e As EventArgs) Handles Button_MoveToRemove.Click
-        If ListBox_UnesedResources.Items.Count > 0 Then
+        If ListBox_UnusedResources.Items.Count > 0 Then
             Dim workingDir As String = Path.GetDirectoryName(OpenFileDialog1.FileName)
             If Not Directory.Exists(workingDir & "\!REMOVE") Then
                 Directory.CreateDirectory(workingDir & "\!REMOVE")
             End If
-            For Each item As String In ListBox_UnesedResources.Items
-                My.Computer.FileSystem.MoveFile(item, workingDir & "\!REMOVE\" & Path.GetFileName(item))
+
+            For Each item As String In ListBox_UnusedResources.Items
+                My.Computer.FileSystem.MoveFile(Path.GetDirectoryName(TextBox_ProjectFilePath.Text) & "\" & item, workingDir & "\!REMOVE\" & Path.GetFileName(item))
             Next
-            ListBox_Resources.Items.Clear()
-            ListBox_UnesedResources.Items.Clear()
-            Label_Resources.Text = "Resources"
-            Label_UnesedResources.Text = "Unesed Resources"
+
+            If File.Exists(OpenFileDialog1.FileName) Then
+                ProjectJSON()
+            End If
+
             MsgBox("Files moved to: " & vbNewLine & workingDir & "\!REMOVE", MsgBoxStyle.Information)
         End If
     End Sub
@@ -43,6 +54,7 @@ Public Class Form1
         If files.Length <> 0 Then
             Try
                 OpenFileDialog1.FileName = files(0)
+                TextBox_ProjectFilePath.Text = files(0)
                 ProjectJSON()
             Catch ex As Exception
                 MsgBox("Problem opening file.", MsgBoxStyle.Critical)
@@ -61,28 +73,38 @@ Public Class Form1
     'ProjectJSON
     Private Sub ProjectJSON()
         ListBox_Resources.Items.Clear()
-        ListBox_UnesedResources.Items.Clear()
-        Dim jsonFile As JObject = JObject.Parse(File.ReadAllText(OpenFileDialog1.FileName))
+        ListBox_UnusedResources.Items.Clear()
+        Label_Resources.Text = "Resources"
+        Label_UnusedResources.Text = "Unused Resources"
+        Dim jsonFile As JObject = JObject.Parse(File.ReadAllText(TextBox_ProjectFilePath.Text))
         For Each resource As JObject In jsonFile.Item("resources")("resources")
             ListBox_Resources.BeginUpdate()
-            ListBox_Resources.Items.Add(Path.GetDirectoryName(OpenFileDialog1.FileName) & "\" & resource.Item("file").ToString.Replace("/", "\"))
+            ListBox_Resources.Items.Add(resource.Item("file").ToString.Replace("/", "\")) 'Path.GetDirectoryName(TextBox_ProjectFilePath.Text) & "\" &
             ListBox_Resources.EndUpdate()
         Next
 
         Dim fileEntries As New ArrayList
-        Dim workingDir As String = Path.GetDirectoryName(OpenFileDialog1.FileName)
-        fileEntries.AddRange(Directory.GetFiles(Path.GetDirectoryName(OpenFileDialog1.FileName), "*.*", SearchOption.AllDirectories).Where(Function(p) Not p.StartsWith(workingDir & "\!REMOVE")).ToArray)
-        fileEntries.Remove(OpenFileDialog1.FileName)
-        fileEntries.Remove(OpenFileDialog1.FileName & ".autosave")
+        Dim workingDir As String = Path.GetDirectoryName(TextBox_ProjectFilePath.Text)
+        Dim bla As String = "test"
+        Dim tempArryList As New ArrayList
+        tempArryList.AddRange(Directory.GetFiles(Path.GetDirectoryName(TextBox_ProjectFilePath.Text), "*.*", SearchOption.AllDirectories).Where(Function(p) Not p.StartsWith(workingDir & "\!REMOVE")).ToArray)
+
+        For index As Integer = 0 To tempArryList.Count - 1
+            tempArryList.Item(index) = tempArryList.Item(index).ToString.Replace(Path.GetDirectoryName(TextBox_ProjectFilePath.Text) & "\", "")
+        Next
+
+        fileEntries.AddRange(tempArryList)
+        fileEntries.Remove(Path.GetFileName(TextBox_ProjectFilePath.Text))
+        fileEntries.Remove(Path.GetFileName(TextBox_ProjectFilePath.Text) & ".autosave")
 
         For Each item As String In fileEntries
             If Not ListBox_Resources.Items.Contains(item) Then
-                ListBox_UnesedResources.BeginUpdate()
-                ListBox_UnesedResources.Items.Add(item)
-                ListBox_UnesedResources.EndUpdate()
+                ListBox_UnusedResources.BeginUpdate()
+                ListBox_UnusedResources.Items.Add(item)
+                ListBox_UnusedResources.EndUpdate()
             End If
         Next
         Label_Resources.Text = "Resources: " & ListBox_Resources.Items.Count
-        Label_UnesedResources.Text = "Unesed Resources: " & ListBox_UnesedResources.Items.Count
+        Label_UnusedResources.Text = "Unused Resources: " & ListBox_UnusedResources.Items.Count
     End Sub
 End Class
